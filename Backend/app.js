f990ab709afb85f582452a6614d1b732767c0dcc
RasmiRanjan03 <sahoo.rasmi.ranjan9827@gmail.com>
2026-01-27 00:24:15 +0530

    Cloudinary and MongoDB and multer Setup

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,21 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectCloudinary from './config/cloudinary.js';
+import connectDB from './config/mongodbconnection.js';
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+connectDB();
+connectCloudinary();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+
 app.get('/', (req, res) => {
-  res.send('Welcome to SmartLib Backend!');
+  res.send('Welcome to SmartLib Backend Rasmi!');
 });
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
